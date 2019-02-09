8415c36809b5a824320d6199b448b1233d8d9e1d
AyushBherwani1998 <34301187+AyushBherwani1998@users.noreply.github.com>
2019-02-09 17:33:06 +0530

    Update homepage.dart

---
+++ collegeconnect_app/lib/user_interface/homepage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:wave/config.dart';
+import 'package:wave/wave.dart';
+
+class Home extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(),
+      
+    );
+  }
+
+}
